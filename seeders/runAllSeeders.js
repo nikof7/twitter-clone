4f6d002663ab073b36c5c0dea75b7a561b124b6d
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-06-30 12:50:52 -0300

    seeder 1

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -16,19 +16,10 @@
 require("dotenv").config();
 
 async function runAllSeeders() {
-  /*
-   * Opcional. Si se quiere borrar toda la base de datos antes ejecutar los
-   * seeders, descomentar las siguientes dos (2) líneas de código.
-   *
-   * PD: El método `dropDatabase` de Mongoose elimina toda la base de datos.
-   */
-  const { mongoose } = require("../db");
-  await mongoose.connection.dropDatabase();
-
   // Seeders:
 
   await require("./userSeeder")();
-  await require("./articleSeeder")();
+  await require("./tweetSeeder")();
 
   /*
    * Aquí se pueden ejectuar otros seeders que hayan en el sistema.
