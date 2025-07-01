31c63336d90d0482f6bdbe7fdcf767c15af54118
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-07-01 19:10:10 -0300

    swagger

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const userRoutes = require("./userRoutes");
 const tweetRoutes = require("./tweetRoutes");
 const authRoutes = require("./authRoutes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+
 module.exports = (app) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use("/users", userRoutes);
   app.use("/tweets", tweetRoutes);
   app.use("/tokens", authRoutes);
